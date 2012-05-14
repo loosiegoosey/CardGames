4065079329e350d6a884dda31b1267d409edd01f
Émerson Hernandez <emerson@MacBook-de-Emerson-Hernandez.local>
2012-05-14 20:11:45 -0300

    Testing Uniritter's ssh

--- src/CardGame.py
+++ src/CardGame.py
@@ -4,7 +4,6 @@ class Card(object):
 
     card_values = {'2':2,'3':3,'4':4,'5':5,'6':6,'7':7,'8':8,'9':9,'10':10,'J':11,'Q':12,'K':13,'A':14}
 
-
     def __init__(self, value, suit):
         self.number = self.card_values[value]
         self.suit = suit
